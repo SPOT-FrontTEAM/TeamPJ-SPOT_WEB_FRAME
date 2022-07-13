97c7fc620f5ec660141259379ec40d023db1c941
Chanho <chjo1245@gmail.com>
2022-07-13 17:50:22 +0900

    Creating Spot information Page

--- client/src/components/views/SpotPage/Spot.js
+++ client/src/components/views/SpotPage/Spot.js
@@ -1,10 +1,18 @@
 import React from "react";
-import NavBar from "../_NavBar/NavBar";
 
 function Spot() {
   return (
     <div className="Spot">
-      Spot
+      <div className="SpotWrapper">
+        <img className="leftItem" src={process.env.PUBLIC_URL + `/assets/centerImg.png`} />
+        <div className="rightItem">
+          <h2>Email: chjo1245@gmail.com</h2>
+          <h2>Email: gkwjd719@naver.com</h2>
+          <h2>Email: eodnjs01477@gmail.com</h2>
+          <h2>Email: rkdgns4566@naver.com</h2>
+          <img src={process.env.PUBLIC_URL + `/assets/SPOTLogo.png`} />
+        </div>
+      </div>
     </div>
   );
 }
